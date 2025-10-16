c2e754c083311deee422cd4e28fccf0c868bc1ca
MrGetsit <85843405+MrGetsit@users.noreply.github.com>
2025-10-16 10:34:26 -0700

    Add files via upload

--- Addons/Splitscreen/Splitscreen.lua
+++ Addons/Splitscreen/Splitscreen.lua
@@ -4,6 +4,10 @@ _addon.commands = { 'splitscreen' }
 
 single_screen = true
 window = false
+character_top_left = 'Spikex'
+character_top_right = 'Sneaksy'
+character_bottom_left = 'Cissilea'
+character_bottom_right = 'Pharen'
 
 windower.register_event('load', function ()
 	windower.send_command('bind %home splitscreen')
@@ -23,21 +27,21 @@ windower.register_event('addon command', function(action)
 			windower.send_command('input /echo Split Screen')
 			windower.send_command('send @all wincontrol resize 960 512')
 			coroutine.sleep(0.1)
-			windower.send_command('send Sneaksy wincontrol move 960 0')
+			windower.send_command('send '..character_top_right..' wincontrol move 960 0')
 			coroutine.sleep(0.1)
-			windower.send_command('send Pharen wincontrol move 960 512')
+			windower.send_command('send '..character_bottom_right..' wincontrol move 960 512')
 			coroutine.sleep(0.1)
-			windower.send_command('send Cissilea wincontrol move 0 512')
+			windower.send_command('send '..character_bottom_left..' wincontrol move 0 512')
 			single_screen = false
 		else
 			windower.send_command('input /echo Single Screen')
 			windower.send_command('send @all wincontrol resize reset')
 			coroutine.sleep(0.1)
-			windower.send_command('send Sneaksy wincontrol move 0 0')
+			windower.send_command('send '..character_top_right..' wincontrol move 0 0')
 			coroutine.sleep(0.1)
-			windower.send_command('send Pharen wincontrol move 0 0')
+			windower.send_command('send '..character_bottom_right..' wincontrol move 0 0')
 			coroutine.sleep(0.1)
-			windower.send_command('send Cissilea wincontrol move 0 0')
+			windower.send_command('send '..character_bottom_left..' wincontrol move 0 0')
 			single_screen = true
 		end 
 	end
